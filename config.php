a10c6f36cec1326dc0296d2886045d6435966217
jorgedv1299 <jorgedv@uninorte.edu.co>
2024-09-18 13:15:32 +0000

    Initial commit

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+header('Content-Type: application/json');
+echo json_encode([
+    'apiKey' => 'AIzaSyBEuRBxTVmq8n8PjWb6ZOVZ4xWW2-JPy_w'
+]);
---
+++ get_location.php
@@ -0,0 +1,43 @@
+<?php
+header('Content-Type: application/json'); // Para que el cliente sepa que está recibiendo JSON
+
+// Configuración de la base de datos
+$servername = "database-1.cdcwiy8egoqg.us-east-1.rds.amazonaws.com";
+$username = "root";
+$password = "15963247";
+$dbname = "gps";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+
+// Obtener la última ubicación
+$sql = "SELECT latitud, longitud, timestamp FROM ubicaciones ORDER BY id DESC LIMIT 1";
+$result = $conn->query($sql);
+
+$data = [];
+if ($result->num_rows > 0) {
+    // Obtener los datos de la última ubicación
+    $row = $result->fetch_assoc();
+    
+    // La fecha ya está en el formato correcto
+    $formattedDate = $row['timestamp'];
+    
+    $data = [
+        'latitud' => $row['latitud'],
+        'longitud' => $row['longitud'],
+        'timestamp' => $formattedDate // Utilizar directamente el timestamp formateado
+    ];
+}
+
+// Cerrar la conexión
+$conn->close();
+
+// Enviar datos en formato JSON
+echo json_encode($data);
+?>
