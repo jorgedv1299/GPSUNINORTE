d230145eaf84b76afb232b1d631a4864d40baa21
dylan0529 <dylan0529@gmail.com>
2024-09-26 01:03:16 +0000

    cambioendpoint

--- sniffer.php
+++ sniffer.php
@@ -23,10 +23,10 @@ date_default_timezone_set('America/Bogota');
 echo "Escuchando en $ip:$port...\n";
 
 
-$servername = "database-1.cdcwiy8egoqg.us-east-1.rds.amazonaws.com"; // Reemplaza con el endpoint de tu RDS si es necesario
-$username = "root";       // Cambia al usuario de tu base de datos
-$password = "15963247";           // Cambia a la contraseña de tu base de datos
-$dbname = "gps";
+$servername = "disenoelec.c98ge4aae1fw.us-east-1.rds.amazonaws.com"; // Reemplaza con el endpoint de tu RDS si es necesario
+$username = "bastod";       // Cambia al usuario de tu base de datos
+$password = "bastod0529";           // Cambia a la contraseña de tu base de datos
+$dbname = "disenoelec";
 
 
 $port = 3306;
