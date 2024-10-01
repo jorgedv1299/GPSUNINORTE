9c26cf29f4e50760e3000edf08f9346e2c9b77ad
alexanderdvm <alexandervelasquezmalo@gmail.com>
2024-10-01 03:08:56 +0000

    p

--- auto_pull.sh
+++ auto_pull.sh
@@ -1,25 +1,12 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 while true; do
     # Cambiar al directorio del repositorio
     cd /var/www/html/GPSUNINORTE || exit
 
     # Hacer pull de los cambios del repositorio remoto
     git pull
-=======
-# Cambiar al directorio del repositorio
-cd /var/www/html/GPSUNINORTE || exit
-
-while true; do
-    # Hacer pull de los cambios del repositorio remoto
-    git pull origin master
->>>>>>> deeeef20d1274c9cf3a13623aafba75240e6e301
 
     # Esperar 5 segundos antes de repetir
     sleep 5
 done
-<<<<<<< HEAD
-=======
-
->>>>>>> deeeef20d1274c9cf3a13623aafba75240e6e301
