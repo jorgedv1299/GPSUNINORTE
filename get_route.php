cd25982ca380068562f0ef746e8deea5efe5672a
alexanderdvm <alexandervelasquezmalo@gmail.com>
2024-10-01 01:00:35 +0000

    Update: se agrego nuevo archivo check_p... el cual recibe del front la consulta de una ubicacion, la compara con las ubicaciones de la base de datos y utilizando la formula de heaverside todas las ubicaciones que esten cerca del lugar, en un rango de 30 metro y  se tomaran como que se paso por ese lugar buscado, en el front se hizo una nueva seccion de busqueda por direccion donde se tiene una casilla con autocompletado de google maps donde se seleccionara el lugar y lo mostrara en el mapa, lo geocodificara en lat y lng y lo enviara al back/check.. para hacer el pasi explicado al principio

--- get_route.php
+++ get_route.php
@@ -2,10 +2,11 @@
 header('Content-Type: application/json');
 
 // Configuración de la base de datos
-$servername = "database-1.cdcwiy8egoqg.us-east-1.rds.amazonaws.com"; // Reemplaza con el endpoint de tu RDS si es necesario
-$username = "root";       // Cambia al usuario de tu base de datos
-$password = "15963247";           // Cambia a la contraseña de tu base de datos
-$dbname = "gps";
+$servername = "alex.cpywocwqwde0.us-east-2.rds.amazonaws.com"; // Reemplaza con el endpoint de tu RDS si es necesario
+$username = "alex";       // Cambia al usuario de tu base de datos
+$password = "alex1234567890";           // Cambia a la contraseña de tu base de datos
+$dbname = "alex";
+
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
