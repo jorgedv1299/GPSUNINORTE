1f72c82b9fd0a88df5534b45affe2940da638904
jorgedv1299 <jorgedv@uninorte.edu.co>
2024-10-03 21:28:22 +0000

    d

--- get_route.php
+++ get_route.php
@@ -7,12 +7,6 @@ $username = "bastod";       // Cambia al usuario de tu base de datos
 $password = "bastod0529";           // Cambia a la contraseña de tu base de datos
 $dbname = "disenoelec";
 
-
-$servername = "disenoelec.c98ge4aae1fw.us-east-1.rds.amazonaws.com"; // Reemplaza con el endpoint de tu RDS si es necesario
-$username = "bastod";       // Cambia al usuario de tu base de datos
-$password = "bastod0529";           // Cambia a la contraseña de tu base de datos
-$dbname = "disenoelec";
-
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
 
