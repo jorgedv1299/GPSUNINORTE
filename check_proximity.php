af8eb7b79932b8f9f416e4a382c119de23093803
jorgedv1299 <jorgedv@uninorte.edu.co>
2024-10-03 16:47:20 +0000

    coreciones

--- check_proximity.php
+++ check_proximity.php
@@ -16,10 +16,10 @@ error_log("Latitud: $lat, Longitud: $lng"); // Registro en el log del servidor
 
 try {
     // Conectar a la base de datos
-    $host = 'alex.cpywocwqwde0.us-east-2.rds.amazonaws.com';
-    $db = 'alex';
-    $user = 'alex';
-    $pass = 'alex1234567890';
+    $host = 'database-1.cdcwiy8egoqg.us-east-1.rds.amazonaws.com';
+    $db = 'gps';
+    $user = 'root';
+    $pass = '15963247';
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Manejo de errores
 
