099ccbdc7329e39b189f7fa54dfdea5f1a747472
jorgedv1299 <jorgedv@uninorte.edu.co>
2024-11-21 20:47:01 +0000

    commit

--- Página_nueva_actualizada/sniffer3.php
+++ Página_nueva_actualizada/sniffer3.php
@@ -19,11 +19,11 @@ date_default_timezone_set('America/Bogota');
 echo "Escuchando en $ip:$port...\n";
 
 // Configuración de la base de datos
-$servername = "alex.cpywocwqwde0.us-east-2.rds.amazonaws.com"; // Reemplaza con el endpoint de tu RDS si es necesario
-$username = "alex";       // Cambia al usuario de tu base de datos
-$password = "alex1234567890";           // Cambia a la contraseña de tu base de datos
-$dbname = "alex";
-
+$servername = "database-1.cdcwiy8egoqg.us-east-1.rds.amazonaws.com"; // Reemplaza con el endpoint de tu RDS si es necesario
+$username = "root";       // Cambia al usuario de tu base de datos
+$password = "15963247";           // Cambia a la contraseña de tu base de datos
+$dbname = "gps";
+$port_db = 3306;
 try {
     // Conectar a la base de datos
     $conn = new mysqli($servername, $username, $password, $dbname, $port_db);
